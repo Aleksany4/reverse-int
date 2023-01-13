3a4de8b99f8cc4bec956bdd1dceed3461e2d77e2
Aleksany4 <aleksany4pl@gmail.com>
2023-01-13 23:23:04 +0100

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+   let str_n = n.toString();
+   let result = '';
+   let i = 0;
+   while (i < str_n.length) {
+    result = str_n[i] + result;
+    i++;
+   }
+   let finish = parseInt(result);
+   return finish;
+};
